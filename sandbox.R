7221cdd26e0338c9b303668fe4febb0ead9b9b83
garrett jaeger <garrett.jaeger@gmail.com>
2018-10-03 19:00:45 -0700

    again

---
+++ sandbox.R
@@ -0,0 +1,16 @@
+#### playing around with our MAUI GitHub repo ####
+#### File construction, data organization & cleaning, and Analyses
+# load packages for use throughout
+library(tidyverse) # suite of sweet packages
+library(readxl) # importing excel files
+library(dplyr) # data transformation
+library(purrr) # for external data manipulation (e.g., merging files)
+library(e1071) # skewness of distribution
+library(pastecs) # quantifying normality
+library(car) # for Levene's test of Homogeneity
+library(ggplot2); library(Hmisc); library(polycor)
+
+## set working directory from which data files are imported and exported
+setwd("~/Documents/GitHub/MAUI") # Mac filepath
+#setwd('C:/Users/gjaeger/Documents/MAUI') # PC filepath (example)
+set.seed(0603) # breadcrumbs for replicability of any randomization that may occur
