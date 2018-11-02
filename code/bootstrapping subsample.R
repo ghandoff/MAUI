f4589f7496b839dd8ebc954720174d8560015fd4
ZachBurns <zachary.c.burns@gmail.com>
2018-11-02 08:44:36 -0700

    added comments & improved organization

--- code/bootstrapping subsample.R
+++ code/bootstrapping subsample.R
@@ -5,17 +5,20 @@ library(readxl)
 
 set.seed(1729)
 
-#' Everything relative to project folder '#
 source('code/bootstrapping subsample functions.R')
 
+##### 
+# Set up/load files
 all_responses <- read_csv('data/Garrett Dissertation Data Answers Only.csv')
 all_responses$Std <- str_replace_all(all_responses$Std, "[^[:alnum:]]", " ") %>% #gets rid of non alphanumerics
   tolower() #' turns everything to lowercase
-
 #' part_flexbyitem is a P x item frame of flex scores
 #' crucially, NA means that the item was not completed by the P
 part_flexbyitem <- std_to_flextable(all_responses, 'TypeItem', 'partID', 'FlexCat')
 
+#####
+# Randomization for target & bootstrap samples
+# Also sets up dfs with bootstrap participants & their responses
 #' column of ID numbers in the target sample
 holdout_nums <- part_flexbyitem %>%
   na.omit() %>% #' ensures target sample did all 9 items
@@ -24,16 +27,19 @@ holdout_nums <- part_flexbyitem %>%
 #' P x item frame of those remaining
 remains_flexbyitem <- part_flexbyitem %>%
   filter(!partID %in% holdout_nums$partID)
-
-items <- names(part_flexbyitem)[-1] #item names from frame
-N <- 100
-curve_formula <- formula(MAUI ~ ((d*(norm_rank)^g)/((d*(norm_rank)^g)+(1-norm_rank)^g)))
-
 #' creates entire list of bootstrapped partIDs
 #' note that not all of them have done all items
 boot_parts <- foreach(i = seq(100, 800, by=100), .combine='rbind') %do% boot_nums(remains_flexbyitem, i)
 boot_list <- bind_rows(holdout_nums, filter(boot_parts, n==0)) #participant IDs of holdout sample & resamples
 
+#####
+# helper formulas, probably can be removed later
+items <- names(part_flexbyitem)[-1] #item names from frame
+N <- 100
+curve_formula <- formula(MAUI ~ ((d*(norm_rank)^g)/((d*(norm_rank)^g)+(1-norm_rank)^g)))
+
+#####
+# scoring of responses, probably will be turned into a function later
 #' creates response tables
 boot_responses <- all_responses %>%
   filter(partID %in% boot_list$partID) #'outputs all responses for all items on boot_list
@@ -41,13 +47,16 @@ boot_response_scores <- foreach(i = items, .combine='rbind') %do% sort_count(boo
 boot_ranks <- foreach(i = items, .combine='rbind') %do% ranks(boot_response_scores, 100, i) #outputs MAUI rank table of bootstrap sample responses
 #boot_calcs <- item_calcs(boot_ranks) #outputs MAUI rank table with 0 and 1 points
 
-
-
 boot_response_scores <- foreach(i = items, .combine='rbind') %do% 
   append_scores(boot_response_scores, boot_ranks, i) #' appends scores
 
 boot_participant_scores <- foreach(i = items, .combine='rbind') %do% 
   p_score(boot_responses, boot_response_scores, i) #' calculates participant scores
+
+
+#####
+
+
 
 #' SAVE SMPL_RANKS HERE FOR GRAPHING
 #' ggplot(data = smpl_ranks, aes(x = MAUI, y = mass)) + geom_col() + xlim(0,1)
