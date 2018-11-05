0204034d694b6f0f687948fa08d9a9a2a975c485
ZachBurns <zcburns@usfca.edu>
2018-11-05 09:49:57 -0800

    new R script for fitting. Also added dissertation data bootstrapping item and participant frames as csvs so we don't have to rerun every time.

---
+++ code/model fitting.R
@@ -0,0 +1,12 @@
+curve_formula <- formula(MAUI ~ ((d*(norm_rank)^g)/((d*(norm_rank)^g)+(1-norm_rank)^g)))
+
+
+#' SAVE SMPL_RANKS HERE FOR GRAPHING
+#' ggplot(data = smpl_ranks, aes(x = MAUI, y = mass)) + geom_col() + xlim(0,1)
+#' ggplot(data = smpl_ranks, aes(x = 1-pct_giving, y = mass)) + geom_col() + geom_vline(aes(xintercept = .95)) + xlim(0,1)
+#' 
+#FIT GAMMA AND DELTA HERE
+fit <- nls(curve_formula, smpl_calcs, start = list(d = .5, g = .6))
+
+s <- filter(all_responses, partID == target_sample$partID[1] & TypeItem == 'U1')
+new <- smpl_itemcount 
