916e6c08d5b0ee347edc53ebf97980c2e73b97b2
ZachBurns <zcburns@usfca.edu>
2018-11-05 13:26:46 -0800

    first attempts at visualizing the bootstrapped results

--- code/model fitting.R
+++ code/model fitting.R
@@ -2,15 +2,64 @@ library(foreach)
 library(tidyverse)
 library(readxl)
 
-source('code/model fitting functions.R')
+#source('code/model fitting functions.R')
 
 ##### 
 # Set up/load files
-item_scores <- read_csv('data/dissertation item scores.csv')
+item_score_frequencies <- read_csv('data/dissertation score frequencies.csv')
 participant_scores <- read_csv('data/dissertation participant scores.csv')
+participant_ids <- read_csv('data/bootstrapped participant ids.csv')
+target_ids <- filter(participant_ids, sample_size == 100)
+target_scores <- filter(participant_scores, partID %in% target_ids$partID)
 
-items <- unique(item_scores$TypeItem) #item names from frame
-item_densities <- foreach(i=items, .combine='rbind') %do% sort_count(item_scores, i)
+items <- unique(item_score_frequencies$TypeItem) #item names from frame
+samples <- unique(item_score_frequencies$sample_size) #sample sizes from frame
+
+#####
+# makes calcs for and graphs density plots
+item_densities <- foreach(i=items, .combine='rbind') %:%
+  foreach(j=samples, .combine='rbind') %do% {
+    hold <- item_score_frequencies %>%
+      filter(TypeItem == i & sample_size == j)
+    max_rank <- nrow(hold)
+    
+    densities <- hold %>%
+      mutate(rank = rank(-count)) %>%
+      mutate(UI95 = case_when(pct_giving <= .05 ~ 1,
+                              TRUE ~ 0)) %>%
+      mutate(norm_rank = (rank-0.5)/max_rank)
+  }
+
+MAUI_item_density_plot <- ggplot(data = item_densities) +
+  geom_line(mapping = aes(x=norm_rank, y=MAUI)) +
+  facet_grid(sample_size ~ TypeItem)
+
+UI95_item_density_plot <- ggplot(data = item_densities) +
+  geom_line(mapping = aes(x=norm_rank, y=UI95)) +
+  facet_grid(sample_size ~ TypeItem)
+
+#####
+# makes calcs for and graphs participant score histograms
+MAUI_participant_hist <- ggplot(data = participant_scores, aes(sum_MAUI)) +
+  geom_histogram() +
+  facet_grid(sample_size ~ TypeItem)
+
+UI95_participant_hist <- ggplot(data = participant_scores, aes(sum_UI95)) +
+  geom_histogram() +
+  facet_grid(sample_size ~ TypeItem)
+
+#####
+# target sample vizualizations
+MAUI_target_hist <- ggplot(data = target_scores, aes(sum_MAUI)) +
+  geom_histogram() +
+  facet_grid(sample_size ~ TypeItem)
+
+UI95_target_hist <- ggplot(data = target_scores, aes(sum_UI95)) +
+  geom_histogram() +
+  facet_grid(sample_size ~ TypeItem)
+
+
+
 
 
 
